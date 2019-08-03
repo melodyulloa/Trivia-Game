e52926cf22b0318d210592b9bda802572ac5a790
MelodyUlloa <melodyalejandra56@gmail.com>
2019-08-03 18:02:55 -0400

    completed based feedback

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,45 @@
+//$(document).ready(function){}
+function startGame(btn){
+  btn.style.display ="none";
+  counter = 10; 
+  currentQuestion = 0;
+  correctAnswers = 0;
+  incorrectAnswers = 0;
+  score = 0;
+  loadQuestion();
+  document.getElementById("container").style.display="block";
+  
+}
 
 const gameQuestions =[{
-    question:"Who is Goku's son?",
-    choices:["Vegeta", "Gohan","Lord Beerus","Android 18"],
-    correctAnswer:"Gohan"
+  question:"Who is Goku's son?",
+  choices:["Vegeta", "Gohan","Lord Beerus","Android 18"],
+  correctAnswer:"Gohan"
 },
 {
-  question:"What's Goku's wife's name?",
-  choices:["Cinderella", "Chi-Chi","Cha-Chi","Bulma"],
-  correctAnswer:"Chi-Chi"
+question:"What's Goku's wife's name?",
+choices:["Cinderella", "Chi-Chi","Cha-Chi","Bulma"],
+correctAnswer:"Chi-Chi"
 },
 {
-    question:"What is the name of Universe 7's God of Destruction?",
-  choices:["Android 17", "Lord Beerus","Lord Champa","Whis"],
-  correctAnswer:"Lord Beerus"
+  question:"What is the name of Universe 7's God of Destruction?",
+choices:["Android 17", "Lord Beerus","Lord Champa","Whis"],
+correctAnswer:"Lord Beerus"
 },
 {
-    question:"Did Master Roshi train Goku and Krillin?",
-  choices:["True","False"],
-  correctAnswer:"True"
+  question:"Did Master Roshi train Goku and Krillin?",
+choices:["True","False"],
+correctAnswer:"True"
 }
 ];
 
 //variables
-var counter = 15;
-var currentQuestion = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var timer;
-var score;
+let counter;
+let currentQuestion;
+let correctAnswers;
+let incorrectAnswers;
+let timer;
+let score;
 
 function nextQuestion() {
   const isQuestionOver = (gameQuestions.length - 1) === currentQuestion;
@@ -47,7 +59,17 @@ function timeUp() {
   clearInterval(timer);
 
   incorrectAnswers++;
-  setTimeout(nextQuestion, 2 * 1000);
+  document.getElementById("feedback").innerHTML = "Incorrect Answer!";
+  document.getElementById("correctAns").innerHTML = gameQuestions[currentQuestion].correctAnswer;
+  document.getElementById("feedback-box").style.display = "block";
+  hideElement("dGame");
+  setTimeout(function(){
+    hideElement("feedback-box");
+    // setTimeout(nextQuestion, 2 * 1000);
+    nextQuestion();
+    showElement("dGame");
+  }, 3000);
+  
 }
 
 
@@ -57,7 +79,9 @@ function countDown(){
     counter--;
 
     $('#time').html('Timer: '+ counter);
-    if(counter === 0){
+    console.log(counter);
+    if(counter <= 0){
+      console.log("counter === 0");
       timeUp();
     }
 }
@@ -65,7 +89,7 @@ function countDown(){
 
 //display question and answer
 function loadQuestion(){
-    counter=15;
+    counter=10;
     timer=setInterval(countDown,1000);
     const question = gameQuestions[currentQuestion].question;
     const choices = gameQuestions[currentQuestion].choices;
@@ -74,7 +98,14 @@ function loadQuestion(){
     ${loadChoices(choices)}
     ${loadRemainingQuestion()}
     `);
-    
+}
+
+function hideElement(id){
+  document.getElementById(id).style.display ="none";
+}
+
+function showElement(id){
+  document.getElementById(id).style.display ="block";
 }
 
 function loadChoices(choices){
@@ -85,7 +116,7 @@ function loadChoices(choices){
 return result;
 }
 
-loadQuestion();
+
 
 
 $(document).on('click', '.choice', function() {
@@ -94,36 +125,59 @@ $(document).on('click', '.choice', function() {
   const correctAnswer = gameQuestions[currentQuestion].correctAnswer;
 
   if (correctAnswer === selectedAnswer) {
+
+
+    console.log(gameQuestions[currentQuestion].correctAnswer);
+    document.getElementById("feedback").innerHTML = "You are Correct!!";
+    document.getElementById("correctAns-wrap").style.display = "none";
+    document.getElementById("feedback-box").style.display = "block";
+    console.log("you are correct");
+    hideElement("dGame");
+    setTimeout(function(){
+      hideElement("feedback-box");
+      // setTimeout(nextQuestion, 2 * 1000);
+      nextQuestion();
+      showElement("dGame");
+    }, 3000);
+
+
+
       score++;
       console.log('Winsss!!!!');
-      setTimeout(nextQuestion, 2 * 1000);
   } else {
       incorrectAnswers++;
-      console.log('Lost!!!!');
-      setTimeout(nextQuestion, 2 * 1000);
+      // console.log('Lost!!!!');
+      // setTimeout(nextQuestion, 2 * 1000);
+      
+      document.getElementById("feedback").innerHTML = "Incorrect Answer!";
+      console.log("you are incorrect");
+      document.getElementById("correctAns-wrap").style.display = "block";
+      document.getElementById("correctAns").innerHTML = gameQuestions[currentQuestion].correctAnswer;
+      document.getElementById("feedback-box").style.display = "block";
+      hideElement("dGame");
+      setTimeout(function(){
+        hideElement("feedback-box");
+        // setTimeout(nextQuestion, 2 * 1000);
+        nextQuestion();
+        showElement("dGame");
+      }, 3000);
+
+      
   }
 });
 
 function displayResult() {
   const result = `
-      <p>You get ${score} questions(s) right</p>
-      <p>You missed ${incorrectAnswers} questions(s)</p>
-      <p>Total questions ${gameQuestions.length} questions(s) right</p>
-      <button class="btn btn-primary" id="reset">Reset Game</button>
+      <p>You get ${score} question(s) right</p>
+      <p>You missed ${incorrectAnswers} question(s)</p>
+      <p>Total questions ${gameQuestions.length} </p>
+      <button class="btn btn-primary" id="reset" onclick="startGame(this)">Reset Game</button>
   `;
-
-  $('#game').html(result);
+   
+  $('#dGame').html(result);
 }
 
-$(document).on('click', '#reset', function() {
-  counter = 15;
-  currentQuestion = 0;
-  score = 0;
-  incorrectAnswers = 0;
-  timer = null;
 
-  loadQuestion();
-});
 
 
 function loadRemainingQuestion() {
@@ -141,8 +195,3 @@ function randomImage(images) {
 }
 
 
-$('#start').click(function() {
-  $('#start').remove();
-  $('#time').html(counter);
-  loadQuestion();
-});
